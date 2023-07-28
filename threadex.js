a8a0ac21a6b38b459d6e1342b9a6dd1bd01448f6
KEggleston <kle1@cornell.edu>
2023-07-28 11:55:25 -0400

    Version 18.1 update

--- threadex.js
+++ threadex.js
@@ -1,5 +1,5 @@
-var latest_version_directory = "data/v17.3",
-	prev_version_directory = "data/v16.3";
+var latest_version_directory = "data/v18.1",
+	prev_version_directory = "data/v17.3";
 /* Standard ACIS data acquisition
 ----------------------------------------------------------*/
 function postSuccess(results, cbf) {
